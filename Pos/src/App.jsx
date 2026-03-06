a30768edea18d5798cc0448503551aba9e2b0ef8
Kuldeep Singh <kuldeepsinghrawat123321@gmail.com>
2026-03-06 21:13:18 +0530

    Weathepos

---
+++ Pos/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import "./App.css";
+export default function App() {
+  return (
+    <div className='mai'>
+      <div className='lb'>
+        <div>H1</div>
+        <div>h2</div>
+        <div>h3</div>
+        <div>h4</div>
+        <div>h5</div>
+      </div>
+      <div className='rb'>
+        <div className='rbtb'>
+        <div>login</div>
+        <div>signup</div>
+        </div>
+        <div className='rbbb'>
+          hey 
+        </div>
+      </div>
+    </div>
+  )
+}
